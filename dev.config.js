7717d808aa5af6fa333c477d10691a5cbd33be09
Matt Ross <mross@innout.com>
2022-12-02 15:40:31 -0800

    Initial commit

---
+++ dev.config.js
@@ -0,0 +1,86 @@
+const fs = require('fs');
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const environmentFilename = "environment.json";
+const sourcePath = "workspaces";
+const entryFilename = "entry.js";
+const scriptFilename = "app-bundle.js";
+const styleFilename = "app-bundle.css";
+
+// Resources larger than 20KB won't be bundled
+const resourceSizeLimit = 20 * 1024;
+
+module.exports = env =>
+{
+    var appEntry = { };
+    var envFile = fs.readFileSync(environmentFilename, 'utf8');
+    var envData = JSON.parse(envFile);
+
+    for(var i = 0; i < envData.projects.length; i++)
+    {
+        appEntry[envData.projects[i].path] = `./${sourcePath}/${envData.projects[i].path}/${entryFilename}`;
+    }
+
+    var exportObj =
+    {
+        entry: appEntry,
+        mode: "development",
+        output:
+        {
+            path: path.resolve(__dirname, sourcePath),
+            filename: `[name]/dist/${scriptFilename}`
+        },
+        optimization:
+        {
+            minimizer:
+            [
+                new TerserJSPlugin({}),
+                new CssMinimizerPlugin({})
+            ]
+        },
+        plugins:
+        [
+            new MiniCssExtractPlugin
+            ({
+                filename: `[name]/dist/${styleFilename}`
+            })
+        ],
+        module:
+        {
+            rules:
+            [
+                {
+                    test: /\.(css)$/i,
+                    use:
+                    [
+                        {
+                            loader: MiniCssExtractPlugin.loader
+                        },
+                        'css-loader'
+                    ]
+                },
+                {
+                    test: /\.(png|gif|jpg|jpeg|ttf|woff2|woff|eot)$/i,
+                    loader: 'url-loader',
+                    options:
+                    {
+                        limit: resourceSizeLimit
+                    }
+                },
+                {
+                    test: /\.(svg)$/i,
+                    loader: 'svg-url-loader',
+                    options:
+                    {
+                        limit: resourceSizeLimit
+                    }
+                }
+            ]
+        }
+    };
+
+    return exportObj;
+};
---
+++ workspaces/project-1/entry.js
@@ -0,0 +1,4 @@
+import './js/page-1.js'
+import './js/page-2.js'
+import './css/page-1.css'
+import './css/page-2.css'
